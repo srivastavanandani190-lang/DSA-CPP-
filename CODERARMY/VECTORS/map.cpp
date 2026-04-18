0c13a92e1ee06d90dc45601bc0c04093b86baa12
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-04-18 14:12:00 +0530

    map

--- CODERARMY/VECTORS/map.cpp
+++ CODERARMY/VECTORS/map.cpp
@@ -10,13 +10,33 @@ int main()
     m["ghi"]=3;
     m["jkl"]=4;
     m["mno"]=5;
-
+    m.insert(make_pair("pqr",6));
+    m.insert(make_pair("abc",7));//not inserted as key is already present
+    m["mno"]=10;//update
     for(auto i:m)
     {
         cout<<i.first<<" "<<i.second<<endl;
     }
+    cout<<m.count("abc");
+    m.erase("def");
+     for(auto i:m)
+    {
+        cout<<i.first<<" "<<i.second<<endl;
+    }   
+    if(m.count("def")==0)
+    {
+        cout<<"not found";
+    }
+    else
+    {
+        cout<<"found";
+    }
+    return 0;
 }
+
 //sorted order-->key value
+//key should be unique
 //implementation-->balanced binary search tree,avl,red black tree
 //search,insertion,delete-->>O(log(n))
 //real life application-->dictionary,phone book,student record,employee record
+//password manager,cache memory,hash table-->unordered map
