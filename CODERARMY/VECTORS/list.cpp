5c564942a8bf0aece552f523fe368cd076ec21ff
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-05-24 14:22:31 +0530

    list

--- CODERARMY/VECTORS/list.cpp
+++ CODERARMY/VECTORS/list.cpp
@@ -11,11 +11,25 @@ int main()
     for (int i : l)
     cout<<i<<" ";
     cout<<endl;
-    // l.pop_back();
-    // l.pop_front();
+  
     cout<<l.front();
-    cout<<l.back();
-     return 0;
+    cout<<l.back()<<endl;
+    l.pop_back();
+    l.pop_front();
+    l.push_back(4);
+    l.push_front(-1);
+    for(auto it = l.begin(); it != l.end(); it++)
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    for(auto it=l.rbegin(); it != l.rend(); it++)
+    {
+        cout<<*it<<" ";
+    }
+
+    return 0;
 }
 // implemented using doubly linked list and it is a sequence container that allows non-contiguous memory allocation.
 // It provides fast insertion and deletion at both ends of the list, but slower access to elements in the middle compared to vectors.
+//auto decides the type of the variable based on the initializer, which can simplify code and improve readability.
