7e6020ab8843786421e2a80919c000520400a12d
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-07-21 10:05:06 +0530

    reverse

--- CW/chapter10(STRINGS)/reverse.cpp
+++ CW/chapter10(STRINGS)/reverse.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
 
 int main(){
 string s ="NANDANI";
 cout<<s<<"\n";
-reverse(s.begin(),s.begin()+3);
+reverse(s.begin(),s.begin()+2);
 cout<<s<<"\n";
 
 return 0;
